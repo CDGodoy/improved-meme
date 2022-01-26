0a4101bea732ab927c2a66d5b3be678dd2f7f7ea
Carlos Nascimento <cdgodoy17@gmail.com>
2022-01-26 11:43:13 -0300

    Grava em CSV

--- bot.py
+++ bot.py
@@ -1,7 +1,7 @@
 from ast import Or
 from cgitb import text
 from multiprocessing.connection import wait
-import os
+import os.path
 import time
 import re
 from datetime import datetime
@@ -23,14 +23,22 @@ class newBot:
         self.driver = webdriver.Chrome(executable_path=r"C:\bin\chromedriver.exe")
     def truckPad(self):
         
+        #Abrindo arquivo para salvar
+        if os.path.exists("truckpad.csv"):
+            arquivo = open("truckpad.csv", 'a')
+        else:
+            arquivo = open("truckpad.csv", 'a')
+            arquivo.write("Origem,Destino,Veiculo,Carroceria,Peso,Preco,Data da Consulta\n")
+
         try:
-            
+            #Formato dos links
+            #https://www.truckpad.com.br/fretes/
+            #https://www.truckpad.com.br/fretes/carga-de-nome-da-cidade-uf/
             #https://www.truckpad.com.br/fretes/carga-de-nome-da-cidade-uf/para-nome-da-cidade-uf/
             site = 'https://www.truckpad.com.br/fretes/carga-de-sao-paulo-sp/para-sao-paulo-sp/'
             self.driver.get(site)
-            self.driver.implicitly_wait(10) #Aguardando para o site carregar
+            self.driver.implicitly_wait(10)
 
-            #/html/body/div[1]/div[1]/div[3]/main/div[2]/h2
             
             itensPagina = self.driver.find_element(By.XPATH, '/html/body/div[1]/div[1]/div[3]/main/div[2]/h2').text
             itensPagina = itensPagina.split()
@@ -42,7 +50,6 @@ class newBot:
                 itensPagina = 20
 
             for j in range(1, itensPagina + 1):
-                print("Entrou no for")
                 
                 origem = self.driver.find_element(By.XPATH, '//*[@id="freights"]/li['+str(j)+']/div[1]/div[1]/div[2]/p[1]/strong').text
                 destino = self.driver.find_element(By.XPATH, '//*[@id="freights"]/li['+str(j)+']/div[1]/div[1]/div[2]/p[2]/strong').text
@@ -51,17 +58,20 @@ class newBot:
                 peso = self.driver.find_element(By.XPATH, '//*[@id="freights"]/li['+str(j)+']/div[1]/div[3]/p[1]/strong').text
                 preco = self.driver.find_element(By.XPATH, '//*[@id="freights"]/li['+str(j)+']/div[1]/div[3]/p[2]/strong[1]').text
                 hora = datetime.strftime(datetime.now(), '%Y%m%d')
+                
+                arquivo.write(origem + ',' + destino + ',' + veiculo + ',' + carroceria + ',' + peso + ',' + preco  + ',' + hora + '\n')
+                #DEBUG
+                # print("-----------------------------------\n")
 
-                print("-----------------------------------\n")
-
-                print("Origem: " + origem)
-                print("Destino: "+ destino)
-                print("Veiculo: "+ veiculo)
-                print("Carroceria: " + carroceria)
-                print("Peso: " + peso)
-                print("Preço: "+ preco)
-                print("Hora: "+ hora)
+                # print("Origem: " + origem)
+                # print("Destino: "+ destino)
+                # print("Veiculo: "+ veiculo)
+                # print("Carroceria: " + carroceria)
+                # print("Peso: " + peso)
+                # print("Preço: "+ preco)
+                # print("Hora: "+ hora)
 
             self.driver.close()        
         except:
             self.driver.close()
+        arquivo.close()
